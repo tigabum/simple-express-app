a2f8e27e8b32271842f6de69bb169dfb5a07b105
Tigabu <tigabu.m@productera.io>
2022-07-18 19:52:32 +0300

    feat: add routes for middleware chain

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog')
 
 var app = express();
 
+// mongodb setup
+
+var mongoose = require('mongoose')
+var mongodb  = 'mongodb+srv://tigmitiku:tigmit@cluster0.pvxe8.mongodb.net/local_library?retryWrites=true&w=majority'
+mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true})
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Mongodb connection error:'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -21,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
