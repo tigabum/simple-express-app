e47bcd5b7dbb86bfa465a842f42e36bb2356efdf
Tigabu <tigabu.m@productera.io>
2022-07-24 20:59:01 +0300

    feat: add genre detail

--- Controllers/genreController.js
+++ Controllers/genreController.js
@@ -1,5 +1,7 @@
 var Genre = require("../models/genre");
-
+var Book = require("../models/book");
+var async = require("async");
+var mongoose = require("mongoose");
 
 // exports.index = (req, res) => {
 //     res.send("index page not implemented")
@@ -7,17 +9,43 @@ var Genre = require("../models/genre");
 
 exports.genre_list = (req, res, next) => {
   Genre.find({})
-  .sort({'name': 'ascending'})
-  .exec(function(err, list_genres){
-    if(err){return next(err) }
-    res.render('genre_list', {title: 'Genre List', genre_list: list_genres})
-  })
+    .sort({ name: "ascending" })
+    .exec(function (err, list_genres) {
+      if (err) {
+        return next(err);
+      }
+      res.render("genre_list", {
+        title: "Genre List",
+        genre_list: list_genres,
+      });
+    });
   // res.send("Not implemented genre list");
 };
 
-// specific book
-exports.genre_detail = (req, res) => {
-  res.send("Not implemented genre", req.params.id);
+// specific book detail
+exports.genre_detail = function (req, res, next) {
+  async.parallel({
+    genre(callback) {
+        Genre.findById(req.params.id)
+          .exec(callback);
+    },
+
+    genre_books(callback) {
+        Book.find({ 'genre': req.params.id })
+          .exec(callback);
+    },
+
+}, function(err, results) {
+    if (err) { return next(err); }
+    if (results.genre==null) { // No results.
+        var err = new Error('Genre not found');
+        err.status = 404;
+        return next(err);
+    }
+    // Successful, so render
+    res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books } );
+});
+  // res.send("Not implemented genre");
 };
 
 // book create form get
--- models/genre.js
+++ models/genre.js
@@ -1,14 +1,15 @@
-var mongoose = require('mongoose')
+var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
 var GenreSchema = new Schema({
-    name: {type: String, required: true, min: 3, max: 100}
-})
+  name: { type: String, required: true },
+});
 
-GenreSchema.virtual('url').get(function(){
-    return '/catalog/genre/ ' + this._id;
-})
+GenreSchema
+.virtual("url")
+.get(function () {
+  return '/catalog/genre/' + this._id;
+});
 
-module.exports = mongoose.model('Genre', GenreSchema)
-
+module.exports = mongoose.model("Genre", GenreSchema);
